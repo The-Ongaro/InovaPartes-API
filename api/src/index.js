e2add5b0186296c9370acd6b90099d5815fac3d8
BRUNO DE OLIVEIRA <brunodeoliveira.22.10@gmail.com>
2023-10-27 08:59:24 -0300

    Update index.js

--- api/src/index.js
+++ api/src/index.js
@@ -3,17 +3,17 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 
-/*
+/**/
 import admController from './controller/admController.js';
 import cartaoController from './controller/cartaoController.js'
 import produtoController from './controller/produtoController.js';
 import usuarioController from './controller/usuarioController.js';
-*/
+
 const server = express();
 server.use(cors());
 server.use(express.json());
 
-/*
+/**/
 server.use('/storage/imgProdutos', express.static('storage/imgProdutos'));
 server.use('/storage/usuarioPerfil', express.static('storage/usuarioPerfil'));
 server.use('/storage/admPerfil', express.static('storage/admPerfil'));
@@ -23,5 +23,5 @@ server.use(admController);
 server.use(cartaoController);
 server.use(produtoController);
 server.use(usuarioController);
-*/
+
 server.listen(process.env.PORT, () => console.log(`API online na porta ${process.env.PORT}`));
