133046d27ceb1b5385b465ababab48d5e16afe9e
BRUNO DE OLIVEIRA <brunodeoliveira.22.10@gmail.com>
2023-10-27 09:04:22 -0300

    ok

--- api/src/repository/connection.js
+++ api/src/repository/connection.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql2/promise';
 
 const conexao = await mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PWD,
-    database: process.env.DB,
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PWD,
+    database: process.env.MYSQL_DB,
     typeCast: function(field, next) {
         if(field.type === 'TINY' && field.length === 1) {
             return field.string() === '1';
