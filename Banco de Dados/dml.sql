eec6c1ecb68b10ff669b9bef5600b1b4c71bf5fd
Maria Alice <ongaromariaalice53@gmail.com>
2023-11-20 19:02:33 -0300

    adição de comando ddl.

--- Banco de Dados/dml.sql
+++ Banco de Dados/dml.sql
@@ -200,6 +200,20 @@ SELECT tb_cliente.id_cliente 		as clienteId,
 		  FROM tb_cartao
 			 INNER JOIN  tb_cliente ON tb_cliente.id_cliente = tb_cartao.id_cliente
 				 ORDER BY IdCliente;
+
+-- LISTAR INFO CARTÃO DE UM DETERMINADO USUÁRIO
+SELECT tb_cliente.id_cliente   as clienteId,
+            tb_cartao.id_cartao     as CartaoId,
+              nm_cliente		    as cliente,
+              ds_cpf			    as cpf,
+              nm_titular		    as titular,
+              ds_cartao			    as cartao,
+              ds_validade  		    as validade,
+              nr_cod_seguranca 	    as codSeguranca,
+              nr_parcelas		    as parcelas
+           FROM tb_cartao
+               INNER JOIN  tb_cliente ON tb_cliente.id_cliente = tb_cartao.id_cliente
+                   WHERE tb_cliente.id_cliente = ?;
         
 -- ALTERAR INFO CARTAO
 UPDATE tb_cartao
